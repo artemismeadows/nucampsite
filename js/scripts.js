6b7049fc22b67e7b4359c0f2ae38299fca10dddf
Artemis Meadows <artemismeadows@gmail.com>
2021-03-13 11:46:23 -0800

    For the memes

--- js/scripts.js
+++ js/scripts.js
@@ -11,4 +11,14 @@ $(function() {
             $("#carouselButton").children("i").addClass("fa-pause"); 
         }
     });
+
+    $(document).on('click', '#reserveButton', function () {
+        //Your function goes here..//
+		$('#reserveModal').modal('show');
+	});
+
+    $(document).on('click', '#loginButton', function () {
+        //Your function goes here..//
+		$('#loginModal').modal('show');
+	});
 });
